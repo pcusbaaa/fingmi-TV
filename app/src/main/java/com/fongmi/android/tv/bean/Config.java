c56ac9e8925957b607f7b10fa60ab9ffdd66cb05
Brian099 <luohuafengchuiqi@163.com>
2025-08-23 09:43:59 +0800

    从环境变量中获取订阅源的base64编码

--- app/src/main/java/com/fongmi/android/tv/bean/Config.java
+++ app/src/main/java/com/fongmi/android/tv/bean/Config.java
@@ -9,6 +9,7 @@ import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
 import com.fongmi.android.tv.App;
+import com.fongmi.android.tv.BuildConfig;
 import com.fongmi.android.tv.db.AppDatabase;
 import com.fongmi.android.tv.utils.FileUtil;
 import com.github.catvod.utils.Path;
