2ade5a6372cec5c552eae58223f1a7f6dd4e9633
Brian099 <luohuafengchuiqi@163.com>
2025-08-26 12:12:31 +0800

    禁止点击点播/直播/壁纸的输入框，即不会弹出源地址，也不在支持从此处获取推送二维码（mobile）

--- app/src/mobile/java/com/fongmi/android/tv/ui/fragment/SettingFragment.java
+++ app/src/mobile/java/com/fongmi/android/tv/ui/fragment/SettingFragment.java
@@ -109,20 +109,20 @@ public class SettingFragment extends BaseFragment implements ConfigCallback, Sit
 
     @Override
     protected void initEvent() {
-        mBinding.vod.setOnClickListener(this::onVod);
-        mBinding.live.setOnClickListener(this::onLive);
-        mBinding.wall.setOnClickListener(this::onWall);
+        //mBinding.vod.setOnClickListener(this::onVod);
+        //mBinding.live.setOnClickListener(this::onLive);
+        //mBinding.wall.setOnClickListener(this::onWall);
         mBinding.cache.setOnClickListener(this::onCache);
         mBinding.backup.setOnClickListener(this::onBackup);
         mBinding.player.setOnClickListener(this::onPlayer);
         mBinding.restore.setOnClickListener(this::onRestore);
         mBinding.version.setOnClickListener(this::onVersion);
-        mBinding.vod.setOnLongClickListener(this::onVodEdit);
-        mBinding.vodHome.setOnClickListener(this::onVodHome);
+        //mBinding.vod.setOnLongClickListener(this::onVodEdit);
+        //mBinding.vodHome.setOnClickListener(this::onVodHome);
         mBinding.live.setOnLongClickListener(this::onLiveEdit);
         mBinding.liveHome.setOnClickListener(this::onLiveHome);
         mBinding.wall.setOnLongClickListener(this::onWallEdit);
-        mBinding.vodHistory.setOnClickListener(this::onVodHistory);
+        //mBinding.vodHistory.setOnClickListener(this::onVodHistory);
         mBinding.version.setOnLongClickListener(this::onVersionDev);
         mBinding.liveHistory.setOnClickListener(this::onLiveHistory);
         mBinding.wallDefault.setOnClickListener(this::setWallDefault);
